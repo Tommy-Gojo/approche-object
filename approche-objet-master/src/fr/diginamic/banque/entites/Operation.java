be2e200a690f1da2ae61324cd7dd90eff55aacca
Tommydeslandes34@gmail.com <FORMATION@DESKTOP-T1FDOG6>
2023-04-12 17:32:02 +0200

    tp08-JAVA

---
+++ approche-objet-master/src/fr/diginamic/banque/entites/Operation.java
@@ -0,0 +1,13 @@
+package fr.diginamic.banque.entites;
+
+public class Operation {
+	String DateOfOp;
+	String MontOfOp;
+	
+	
+	public Operation(String dateOfOp, String montOfOp) {
+		super();
+		DateOfOp = dateOfOp;
+		MontOfOp = montOfOp;
+	}
+}
